4865b52716cdb7d1b0abc3200313884ba35969e7
Yogesh Darji <yogesh_darji99@yahoo.com>
2017-09-26 11:32:52 -0500

    Added DeleteStudent Query and Update query

--- hibernate-tutorial/src/com/yogesh/hibernate/demo/DeleteStudentDemo.java
+++ hibernate-tutorial/src/com/yogesh/hibernate/demo/DeleteStudentDemo.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 
 import com.yogesh.hibernate.demo.entity.Student;
 
-public class UpdateStudentDemo {
+public class DeleteStudentDemo {
 
 	public static void main(String[] args) {
 		
@@ -31,26 +31,16 @@ public class UpdateStudentDemo {
 			
 			Student myStudent = session.get(Student.class, studentId);
 			
-			System.out.println("Updating student...");
+			//delete the student
+			//System.out.println("Deleting the student: "+ myStudent);
+			//session.delete(myStudent);
 			
-			myStudent.setFirstName("Scooby");
+			//delete student id=2
+			System.out.println("Deleting student id=2");
+			
+			session.createQuery("delete from Student where id=2").executeUpdate();
 			
 			//commit the transaction
-			session.getTransaction().commit();
-			
-			//NEW Code
-			
-			session = factory.getCurrentSession();
-			session.beginTransaction();
-			
-			//update email for all students
-			System.out.println("Updating email for all students");
-			
-			session.createQuery("update Student set email='foo@gmail.com'")
-					.executeUpdate();
-					
-			
-			//commit transaction
 			session.getTransaction().commit();
 			
 			System.out.println("Done!");			
